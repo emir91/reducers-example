592e8281baddebaea47f46c17aa5fe3ed843e849
emir.vranac <vranacemir@gmail.com>
2022-04-24 09:46:50 +0200

    create reducer, Puzzle and style files

---
+++ src/Puzzle.js
@@ -0,0 +1,9 @@
+import React, { useReducer } from "react"
+import reducer from "./reducer"
+import "./assets/styles/puzzle.css"
+
+const Puzzle = () => {
+  return <div></div>
+}
+
+export default Puzzle
