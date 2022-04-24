d0889b9025111b652e91746ac66c2c15ab6bf647
emir.vranac <vranacemir@gmail.com>
2022-04-24 11:36:36 +0200

    add useUndoReducer hook to the Puzzle

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,9 +1,10 @@
-import React, { useReducer } from "react"
+import React from "react"
 import reducer from "./reducer"
+import useUndoReducer from "./useUndoReducer"
 import "./assets/styles/puzzle.css"
 
 const Puzzle = () => {
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, dispatch] = useUndoReducer(reducer, {
     items: ["4", "1", "2", "7", "6", "3", null, "5", "8"],
   })
   return (
@@ -33,6 +34,20 @@ const Puzzle = () => {
           onClick={() => dispatch({ type: "reset" })}
         >
           Reset
+        </button>
+      </div>
+      <div className='puzzle-controls'>
+        <button
+          className='puzzle-shuffle'
+          onClick={() => dispatch({ type: "undo" })}
+        >
+          Undo
+        </button>
+        <button
+          className='puzzle-reset'
+          onClick={() => dispatch({ type: "redo" })}
+        >
+          Redo
         </button>
       </div>
       {state.complete && <div className='puzzle-complete'>Complete!</div>}
